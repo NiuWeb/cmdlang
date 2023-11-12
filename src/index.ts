dac397da7816fea7f74b884a6e3a0543b98c9b16
NiuWeb <niuweb1channel@gmail.com>
2023-11-12 18:10:09 -0500

    expose logger class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./cmd/command"
+export { Logger } from "./logger/Logger"
 export { Compiler } from "./compiler/Compiler"
 export { Parser as LangParser } from "./compiler/Parser"
 export { Parser as ExprParser } from "@bygdle/expr-parser"
