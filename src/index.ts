41418ee9df169f538a9a4c8aaace61b174c6c0b6
NiuWeb <niuweb1channel@gmail.com>
2023-11-12 14:53:44 -0500

    configure lib entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("testing")
+export { Program } from "./cmd/command/Program"
+export { Compiler } from "./compiler/Compiler"
+export { Parser as LangParser } from "./compiler/Parser"
+export { Parser as ExprParser } from "@bygdle/expr-parser"
