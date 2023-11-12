054f12435639c39df57105225aafa328935105d8
NiuWeb <niuweb1channel@gmail.com>
2023-11-12 16:44:19 -0500

    Create logger class

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -1,5 +1,66 @@
+import { Location } from "@bygdle/expr-parser"
+import { sprintf } from "sprintf-js"
+
+export interface Log {
+    type: "log" | "warn" | "error"
+    message: string
+    line: number
+}
+
 export class Logger {
     public static Global = new Logger()
 
-    
+    private _log: Log[] = []
+    private line = 1
+
+    public out = console.log
+
+    private addLog(log: Log) {
+        this._log.push(log)
+        this.out(`[${log.type} at line ${log.line}] ${log.message}`)
+    }
+
+    public setLine(line: number | Location) {
+        if (typeof line === "number") {
+            this.line = line
+        } else {
+            this.line = line[0]
+        }
+    }
+
+    public log(msg: string) {
+        this.addLog({
+            type: "log",
+            message: msg,
+            line: this.line
+        })
+    }
+
+    public logf(msg: string, ...format: unknown[]) {
+        this.log(sprintf(msg, ...format))
+    }
+
+    public warn(msg: string) {
+        this.addLog({
+            type: "warn",
+            message: msg,
+            line: this.line
+        })
+    }
+
+    public warnf(msg: string, ...format: unknown[]) {
+        this.warn(sprintf(msg, ...format))
+    }
+
+    public error(msg: string) {
+        this.addLog({
+            type: "error",
+            message: msg,
+            line: this.line
+        })
+    }
+
+    public errorf(msg: string, ...format: unknown[]) {
+        this.error(sprintf(msg, ...format))
+    }
 }
