e6da35cb930b6718075486091532f87f28cbfa40
NiuWeb <niuweb1channel@gmail.com>
2023-11-13 10:45:00 -0500

    expose removeComments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./cmd/command"
 export { Logger } from "./logger/Logger"
 export { Compiler } from "./compiler/Compiler"
+export { RemoveComments } from "./compiler/comments"
 export { Parser as LangParser } from "./compiler/Parser"
 export { Parser as ExprParser } from "@bygdle/expr-parser"
 export * from "./globals"
