633a4e0aa0ad65caf24f222df8e30e806f9d37b9
NiuWeb <niuweb1channel@gmail.com>
2023-11-16 15:06:07 -0500

    add comments

--- src/cmd/arguments/ArgumentGetter.ts
+++ src/cmd/arguments/ArgumentGetter.ts
@@ -6,6 +6,10 @@ export class ArgumentGetter {
 
     constructor(public list: ArgumentList, public expressions: (Expression | undefined)[]) {}
 
+    /**
+     * Gets the numeric value of an argument at the given position,
+     * or with the given name. Throws if the argument is not a number.
+     */
     public number(nameOrIndex: number | string) {
         const index = this.list.index[nameOrIndex]
         const expr = this.expressions[index]
@@ -14,6 +18,9 @@ export class ArgumentGetter {
         return toNumber(this.list.named[nameOrIndex])
     }
 
+    /**
+     * Gets the expression at the given position.
+     */
     public expression(nameOrIndex: number | string) {
         const index = this.list.index[nameOrIndex]
         return this.expressions[index]
