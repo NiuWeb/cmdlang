d5b3fc7c938d63cdae736501d650e2836263f884
NiuWeb <niuweb1channel@gmail.com>
2024-01-07 13:03:44 -0500

    remove esm->cjs transform for my own modules in jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
+    "^@src/(.*)$": "<rootDir>/src/$1",
   },
-  transform: {
-    "node_modules/((@bygdle.*)/)": "esbuild-jest",
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!(@bygdle.*)/)"
-  ],
-};
+}
